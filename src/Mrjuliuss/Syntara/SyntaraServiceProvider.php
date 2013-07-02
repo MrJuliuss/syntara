8c6fc489a83031061b56616173c8433d12f1de02
julienr <julien.richarte@gmail.com>
2013-07-02 16:57:18 +0200

    [Upg] update service provider

--- src/Mrjuliuss/Syntara/SyntaraServiceProvider.php
+++ src/Mrjuliuss/Syntara/SyntaraServiceProvider.php
@@ -11,15 +11,20 @@ class SyntaraServiceProvider extends ServiceProvider {
 	 */
 	protected $defer = false;
 
+	public function boot() 
+	{
+		$this->package('mrjuliuss/syntara');
+
+		// include start file
+		include ( __DIR__ . '/../../start.php');
+	}
+
 	/**
 	 * Register the service provider.
 	 *
 	 * @return void
 	 */
-	public function register()
-	{
-		//
-	}
+	public function register(){}
 
 	/**
 	 * Get the services provided by the provider.
