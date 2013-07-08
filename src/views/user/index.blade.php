36c876d23a6c71f515bb429e6c09b58dd786f267
julienr <julien.richarte@gmail.com>
2013-07-08 13:54:33 +0200

    [Upg] test table bootstrap for users list

--- src/views/user/index.blade.php
+++ src/views/user/index.blade.php
@@ -3,39 +3,36 @@
 @section('content')
 {{ Breadcrumbs::create(array(array('title' => 'Users', 'link' => 'dashboard/users', 'icon' => 'icon-user'))); }}
 <div class="span10">
-		<table class="table table-bordered">
-			<thead>
-				<tr role="row">
-					<th style="width:20px;">
-						<div>Id</div>
-					</th>
-					<th style="width: 304px;">
-						<div>Email</div>
-					</th>
-					<th style="width: 300px;">
-						<div >Last Name</div>
-					</th>
-					<th style="width: 273px;">
-						<div >First Name</div>
-					</th>
-				</tr>
-			</thead>
+	<br>
+	<table class="table table-bordered">
+		<thead>
+			<tr role="row">
+				<th style="width:20px;">
+					<div>Id</div>
+				</th>
+				<th style="width: 304px;">
+					<div>Email</div>
+				</th>
+				<th style="width: 300px;">
+					<div >Last Name</div>
+				</th>
+				<th style="width: 273px;">
+					<div >First Name</div>
+				</th>
+			</tr>
+		</thead>
 
-			@foreach ($users as $user)
-				<tbody>
-				<tr>
-					<td><input type="checkbox" ></td>
-					<td>{{ $user->getId() }}</td>
-					<td>{{ $user->getLogin() }}</td>
-					<td>Test</td>
-				</tr>
-				</tbody>
-			@endforeach
-		</table>
-		{{ $users->links() }}
+		@foreach ($users as $user)
+			<tbody>
+			<tr>
+				<td><input type="checkbox" ></td>
+				<td>{{ $user->getId() }}</td>
+				<td>{{ $user->getLogin() }}</td>
+				<td>Test</td>
+			</tr>
+			</tbody>
+		@endforeach
+	</table>
+	{{ $users->links() }}
 </div>
-
-
-
-
 @stop
