eb12838a167025fda8605c6939f775cee89ad0fc
MrJuliuss <julien.richarte@gmail.com>
2013-07-15 20:33:24 +0200

    [Upg] update new user view

--- src/views/user/new.blade.php
+++ src/views/user/new.blade.php
@@ -6,6 +6,29 @@
 
 <section class="module" style="margin: 20px;">
     <div class="module-head">
+        <b>New user</b>
+    </div>
+    <div class="module-body">
+        <form action="" method="POST">
+            <div class="span4">
+                <div class="control-group">
+                    <label class="control-label">Account informations</label>
+                    <div class="controls">
+                        <p><input class="input-xlarge" type="text" placeholder="Email"></p>
+                        <p><input class="input-xlarge" type="text" placeholder="Password"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">Profile informations</label>
+                <div class="controls">
+                    <p><input class="input-xlarge" type="text" placeholder="Last name"></p>
+                    <p><input class="input-xlarge" type="text" placeholder="First name"></p>
+                </div>
+            </div>
+            <br>
+            <button class="btn btn-primary">Create</button>
+        </form>
     </div>
 </section>
 @stop
