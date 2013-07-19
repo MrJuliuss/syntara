e3fe5f432ba3c5d8fcf7e66adeabb25386d94706
MrJuliuss <julien.richarte@gmail.com>
2013-07-19 22:31:36 +0200

    [Upg] bugfix responsive

--- src/views/user/show.blade.php
+++ src/views/user/show.blade.php
@@ -4,48 +4,49 @@
 <link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/dashboard/users.css') }}" />
 {{ Breadcrumbs::create(array(array('title' => 'Users', 'link' => "dashboard/users", 'icon' => 'icon-user'))); }}
 
-<section class="module">
-    <div class="module-head">
-        <b>{{ $user->getId() }} - {{ $user->username }}</b>
-    </div>
-    <div class="module-body ajax-content">
-        <div class="row-fluid">
-            <div class="span6">
+<div class="container-fluid">
+<div class="row-fluid">
+    <div class="span6">
+        <section class="module">
+            <div class="module-head">
+                <b>{{ $user->getId() }} - {{ $user->username }}</b>
+            </div>
+            <div class="module-body ajax-content">
                 <form class="form-horizontal">
                      <div class="control-group">
                         <label class="control-label">Username :</label>
                         <div class="controls">
-                            <input class="input-xlarge" type="text" id="userName" name="userName" value="{{ $user->username}}">
+                            <input class="span12" type="text" id="userName" name="userName" value="{{ $user->username}}">
                         </div>
                     </div>
                     <div class="control-group">
                         <label class="control-label">Email :</label>
                         <div class="controls">
-                            <input class="input-xlarge" type="text" id="userEmail" name="userEmail" value="{{ $user->email }}">
+                            <input class="span12" type="text" id="userEmail" name="userEmail" value="{{ $user->email }}">
                         </div>
                     </div>
                     <div class="control-group">
                         <label class="control-label">Password :</label>
                         <div class="controls">
-                            <input class="input-xlarge" type="password" placeholder="Password" id="userPass" name="userPass" >
+                            <input class="span12" type="password" placeholder="Password" id="userPass" name="userPass" >
                         </div>
                     </div>
                     <div class="control-group">
                         <label class="control-label">Password again :</label>
                         <div class="controls">
-                            <input class="input-xlarge" type="password"  id="userPassAgain" name="userPassAgain" >
+                            <input class="span12" type="password"  id="userPassAgain" name="userPassAgain" >
                         </div>
                     </div>
                     <div class="control-group">
                         <label class="control-label">Last name :</label>
                         <div class="controls">
-                            <input class="input-xlarge" type="text" id="userLastName" name="userLastName" value="{{ $user->last_name  }}">
+                            <input class="span12" type="text" id="userLastName" name="userLastName" value="{{ $user->last_name  }}">
                         </div>
                     </div>
                     <div class="control-group">
                         <label class="control-label">First name :</label>
                         <div class="controls">
-                            <input class="input-xlarge" type="text" id="userFirstName" name="userFirstName" value="{{ $user->first_name }}">
+                            <input class="span12" type="text" id="userFirstName" name="userFirstName" value="{{ $user->first_name }}">
                         </div>
                     </div>
                     <div class="control-group">
@@ -55,24 +56,31 @@
                     </div>
                 </form>
             </div>
-            <div>
-                <div class="control-group">
-                    <div class="controls">
-                        <label>Registration : </label><p>{{ $user->created_at }}</p> 
-                    </div>   
+        </section>
+    </div>
+    <div class="span6">
+        <section class="module">
+        <div class="module-head">
+            <b>Informations</b>
+        </div>
+        <div class="module-body ajax-content">
+            <div class="control-group">
+                <div class="controls">
+                    <label>Registration : </label><p>{{ $user->created_at }}</p> 
                 </div>
-                <div class="control-group">
-                    <div class="controls">
-                        <label>Last update : </label><p>{{ $user->updated_at }}</p>
-                    </div>   
-                </div>
-                <div class="control-group">
-                    <div class="controls">
-                        <label>Last login : </label><p>{{ $user->last_login }}</p>
-                    </div>   
-                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <label>Last update : </label><p>{{ $user->updated_at }}</p>
+                </div>   
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <label>Last login : </label><p>{{ $user->last_login }}</p>
+                </div>   
             </div>
         </div>
     </div>
-</section>
+</div>
+</div>
 @stop
