f24ed14eb115bfa42776bebc21e085fc3a8f186c
MrJuliuss <julien.richarte@gmail.com>
2013-07-20 12:22:38 +0200

    [Add] add error page

---
+++ src/views/dashboard/error.blade.php
@@ -0,0 +1,25 @@
+@extends('syntara::layouts.dashboard.master')
+
+@section('content')
+<link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/dashboard/users.css') }}" />
+<script src="{{ asset('packages/mrjuliuss/syntara/assets/js/dashboard/user.js') }}"></script> 
+<script src="{{ asset('packages/mrjuliuss/syntara/assets/js/forms/check.js') }}"></script> 
+{{ Breadcrumbs::create(array(array('title' => 'Dashboard', 'link' => "dashboard", 'icon' => 'icon-home'))); }}
+
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span6 offset3">
+            <section class="module">
+                <div class="module-head">
+                    <b>Error !</b>
+                </div>
+                <div class="module-body">
+                    <div class="alert alert-error">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+@stop
