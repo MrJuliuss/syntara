3c9c67b98125d73c6413a6d028f72c5b6a131d63
MrJuliuss <julien.richarte@gmail.com>
2013-07-20 12:24:05 +0200

    [Upg] responsive design bugfix

--- src/views/user/index.blade.php
+++ src/views/user/index.blade.php
@@ -3,13 +3,16 @@
 @section('content')
 <link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/dashboard/users.css') }}" />
 {{ Breadcrumbs::create(array(array('title' => 'Users', 'link' => "dashboard/users", 'icon' => 'icon-user'))); }}
-
-<section class="module">
-    <div class="module-head">
-        <b>All users</b>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <section class="module">
+            <div class="module-head">
+                <b>All users</b>
+            </div>
+            <div class="module-body ajax-content">
+                @include('syntara::user.list-users')
+            </div>
+        </section>
     </div>
-    <div class="module-body ajax-content">
-        @include('syntara::user.list-users')
-    </div>
-</section>
+</div>
 @stop
--- src/views/user/new.blade.php
+++ src/views/user/new.blade.php
@@ -6,36 +6,40 @@
 <script src="{{ asset('packages/mrjuliuss/syntara/assets/js/forms/check.js') }}"></script> 
 {{ Breadcrumbs::create(array(array('title' => 'Users', 'link' => "dashboard/users", 'icon' => 'icon-user'), array('title' => 'New user', 'link' => "dashboard/user/new", 'icon' => 'icon-plus-sign'))); }}
 
-<section class="module">
-    <div class="module-head">
-        <b>New user</b>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <section class="module">
+            <div class="module-head">
+                <b>New user</b>
+            </div>
+            <div class="module-body">
+                <form id="create-user-form" action="" method="POST">
+                    <div class="row-fluid">
+                        <div class="span4">
+                            <div class="control-group">
+                                <label class="control-label">Account informations</label>
+                                <div class="controls">
+                                    <p><input class="input-xxlarge" type="text" placeholder="User name" id="userName" name="userName"></p>
+                                    <p><input class="input-xxlarge" type="text" placeholder="Email" id="userEmail" name="userEmail"></p>
+                                    <p><input class="input-xxlarge" type="password" placeholder="Password" id="userPass" name="userPass"></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="span4">
+                            <div class="control-group">
+                                <label class="control-label">Profile informations</label>
+                                <div class="controls">
+                                    <p><input class="input-xxlarge" type="text" placeholder="Last name" id="userLastName" name="userLastName"></p>
+                                    <p><input class="input-xxlarge" type="text" placeholder="First name" id="userFirstName" name="userFirstName"></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <button id="add-user" class="btn btn-primary">Create</button>
+                </form>
+            </div>
+        </section>
     </div>
-    <div class="module-body">
-        <form id="create-user-form" action="" method="POST">
-			<div class="row-fluid">
-				<div class="span4">
-					<div class="control-group">
-						<label class="control-label">Account informations</label>
-						<div class="controls">
-							<p><input class="input-xxlarge" type="text" placeholder="User name" id="userName" name="userName"></p>
-							<p><input class="input-xxlarge" type="text" placeholder="Email" id="userEmail" name="userEmail"></p>
-							<p><input class="input-xxlarge" type="password" placeholder="Password" id="userPass" name="userPass"></p>
-						</div>
-					</div>
-				</div>
-				<div class="span4">
-					<div class="control-group">
-						<label class="control-label">Profile informations</label>
-						<div class="controls">
-							<p><input class="input-xxlarge" type="text" placeholder="Last name" id="userLastName" name="userLastName"></p>
-							<p><input class="input-xxlarge" type="text" placeholder="First name" id="userFirstName" name="userFirstName"></p>
-						</div>
-					</div>
-				</div>
-			</div>
-            <br>
-            <button id="add-user" class="btn btn-primary">Create</button>
-        </form>
-    </div>
-</section>
+</div>
 @stop
