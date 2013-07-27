885c61f7a6a69d2cfba428de3a626bb253dc7dd3
MrJuliuss <julien.richarte@gmail.com>
2013-07-27 23:09:26 +0200

    [Upg] merge conflict

--- public/assets/js/dashboard/login.js
+++ public/assets/js/dashboard/login.js
@@ -2,9 +2,6 @@ $(document).ready(function()
 {
     $('#login-form').on('submit', function() 
     {
-        if(!checkLoginFormInput())
-            return false;
-        
         $.ajax({
             "type": "POST",
             "url": "login",
@@ -12,13 +9,20 @@ $(document).ready(function()
             "dataType": "json",
             success: function(result) 
             { 
-                if(result.logged === true)
+                if(result.userCreated === false)
                 {
-                    window.location = "";
+                    if(typeof result.errorMessage !== 'undefined')
+                    {
+                        showStatusMessage(result.errorMessage, 'error');
+                    }
+                    else if(typeof result.errorMessages !== 'undefined')
+                    {
+                        showRegisterFormAjaxErrors(result.errorMessages);
+                    }
                 }
                 else
                 {
-                    showRegisterFormAjaxErrors(result.errorMessages);
+                    window.location = "";
                 }
             }
         });
@@ -26,27 +30,3 @@ $(document).ready(function()
         return false;
     });
 });
-
-var checkLoginFormInput = function()
-{
-    $('.label-important').remove();
-    var errors = new Array();
-    if($('#email').val() === "")
-    {
-        errors['email'] = new Array('Please enter your login');
-    }
-    if($('#pass').val() === "")
-    {
-        errors['pass'] = new Array('Please enter your password');
-    }
-    
-    if(Object.keys(errors).length)
-    {
-        showRegisterFormAjaxErrors(errors);
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-};
