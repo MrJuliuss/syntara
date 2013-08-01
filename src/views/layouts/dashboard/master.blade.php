2efaceef1053c7f23c64856f846d65b7b190d265
julienr <julien.richarte@gmail.com>
2013-08-01 13:27:17 +0200

    [Upg] refactoring js

--- src/views/layouts/dashboard/master.blade.php
+++ src/views/layouts/dashboard/master.blade.php
@@ -2,7 +2,6 @@
     <head>
         <link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/bootstrap.min.css') }}" />
         <link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/base.css') }}" media="all">
-        <link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/base.responsive.css') }}" media="all">
 
         <script src="{{ asset('packages/mrjuliuss/syntara/assets/js/jquery-2.0.3.min.js') }}"></script> 
         <script src="{{ asset('packages/mrjuliuss/syntara/assets/js/bootstrap.min.js') }}"></script> 
