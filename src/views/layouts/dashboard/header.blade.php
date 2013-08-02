01f5ffc7a6dabaca91144e1dc82e7f7751e716ae
MrJuliuss <julien.richarte@gmail.com>
2013-08-02 19:36:09 +0200

    [Upg] refactoring navbar

--- src/views/layouts/dashboard/header.blade.php
+++ src/views/layouts/dashboard/header.blade.php
@@ -1,32 +1,29 @@
 <div class="navbar main-bar">
-        
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Syntara Dashboard</a>
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Syntara Dashboard</a>
+    <ul class="nav navbar-nav">
+        <li class=""><a href="{{ URL::to('dashboard'); }}"><i class="icon icon-home"></i> <span>Dashboard</span></a></li>
         @if (Sentry::check())
-        <div class="nav-collapse collapse navbar-responsive-collapse">
-            <ul class="nav navbar-nav pull-right">
-                <li class="" ><a title="" href="{{ URL::to('dashboard/profile'); }}"><i class="icon icon-user"></i> <span class="text">Profile</span></a></li>
-                <li class=""><a title="" href="{{ URL::to('dashboard/logout'); }}"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Users</a>
+            <ul class="dropdown-menu">
+                <li><a href="{{ URL::to('dashboard/users'); }}">Users</a></li>
+                <li><a href="{{ URL::to('dashboard/groups'); }}">Groups</a></li>
             </ul>
-        </div>
+        </li>
         @endif
-    </div>
+    </ul>
 
-<div class="navbar sub-bar">
-	<ul class="nav navbar-nav">
-		<li class=""><a href="{{ URL::to('dashboard'); }}"><i class="icon icon-home"></i> <span>Dashboard</span></a></li>
-		@if (Sentry::check())
-		<li class="dropdown">
-			<a class="dropdown-toggle" data-toggle="dropdown" href="#">Users</a>
-			<ul class="dropdown-menu">
-				<li><a href="{{ URL::to('dashboard/users'); }}">Users</a></li>
-				<li><a href="{{ URL::to('dashboard/groups'); }}">Groups</a></li>
-			</ul>
-		</li>
-		@endif
-	</ul>
+    @if (Sentry::check())
+    <div class="nav-collapse collapse navbar-responsive-collapse">
+        <ul class="nav navbar-nav pull-right">
+            <li class="" ><a title="" href="{{ URL::to('dashboard/profile'); }}"><i class="icon icon-user"></i> <span class="text">Profile</span></a></li>
+            <li class=""><a title="" href="{{ URL::to('dashboard/logout'); }}"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+        </ul>
+    </div>
+    @endif
 </div>
