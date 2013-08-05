5373314f78e5c564943e31c5d6337a433c290f17
MrJuliuss <julien.richarte@gmail.com>
2013-08-05 23:55:45 +0200

    [Upg] refatoring divers

--- src/controllers/GroupController.php
+++ src/controllers/GroupController.php
@@ -30,66 +30,59 @@ class GroupController extends BaseController {
     */
     public function postCreate()
     {
-        try
-        {
-			$permissionsValues = Input::get('permission');
-            $groupname = Input::get('groupname');
-			$permissionErrors = array();
-            $permissions = array();
-            
-            // validate permissions
-            foreach($permissionsValues as $key => $permission)
-            {
-                $validPermission = Validator::make(array('permission' => $permission), Config::get('syntara::rules.groups.create_permission'));
-                if($validPermission->fails())
-                {
-                    $permissionErrors['permission['.$key.']'] = $validPermission->messages()->getMessages();
-                }
-                else
-                {
-                   $permissions[$permission] = 1;
-                }
-            }
-            
-            // validate group name
-            $validator = Validator::make(
-                array('groupname' => $groupname),
-                Config::get('syntara::rules.groups.create_name')
-            );
+        $permissionsValues = Input::get('permission');
+        $groupname = Input::get('groupname');
+        $permissionErrors = array();
+        $permissions = array();
 
-            $gnErrors = array();
-            if($validator->fails())
-            {
-                $gnErrors = $validator->messages()->getMessages();
-            }
-            
-			$errors = array_merge($permissionErrors, $gnErrors); 
-            if(!empty($errors))
-            {
-                return Response::json(array('groupCreated' => false, 'errorMessages' => $errors));
-            }
-            else 
-            {
-                try
-                {
-                    Sentry::getGroupProvider()->create(array(
-                        'name' => $groupname,
-                        'permissions' => $permissions,
-                    ));
-                }
-                catch (\Cartalyst\Sentry\Groups\NameRequiredException $e) {}
-                catch (\Cartalyst\Sentry\Groups\GroupExistsException $e)
-                {
-                    return Response::json(array('groupCreated' => false, 'errorMessage' => 'Group with this name already exists.'));
-                }
-            }
-            
-            return Response::json(array('groupCreated' => true));
-        }
-        catch(Exception $e)
+        // validate permissions
+        foreach($permissionsValues as $key => $permission)
         {
-            
+            $validPermission = Validator::make(array('permission' => $permission), Config::get('syntara::rules.groups.create_permission'));
+            if($validPermission->fails())
+            {
+                $permissionErrors['permission['.$key.']'] = $validPermission->messages()->getMessages();
+            }
+            else
+            {
+               $permissions[$permission] = 1;
+            }
         }
+
+        // validate group name
+        $validator = Validator::make(
+            array('groupname' => $groupname),
+            Config::get('syntara::rules.groups.create_name')
+        );
+
+        $gnErrors = array();
+        if($validator->fails())
+        {
+            $gnErrors = $validator->messages()->getMessages();
+        }
+
+        $errors = array_merge($permissionErrors, $gnErrors); 
+        if(!empty($errors))
+        {
+            return Response::json(array('groupCreated' => false, 'errorMessages' => $errors));
+        }
+        else 
+        {
+            try
+            {
+                Sentry::getGroupProvider()->create(array(
+                    'name' => $groupname,
+                    'permissions' => $permissions,
+                ));
+            }
+            catch (\Cartalyst\Sentry\Groups\NameRequiredException $e) {}
+            catch (\Cartalyst\Sentry\Groups\GroupExistsException $e)
+            {
+                return Response::json(array('groupCreated' => false, 'errorMessage' => 'Group with this name already exists.'));
+            }
+        }
+
+        return Response::json(array('groupCreated' => true));
     }
     
     /**
