67eaba022b84b57e38d3c7266a68ef5a266ecc70
Dan Sullivan <dan@embu.co.uk>
2013-08-28 16:44:01 +0100

    Fix glyphicons

--- src/views/layouts/dashboard/master.blade.php
+++ src/views/layouts/dashboard/master.blade.php
@@ -1,7 +1,6 @@
 <html>
     <head>
         <link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/bootstrap.min.css') }}" />
-        <link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/bootstrap-glyphicons.css') }}" />
         <link rel="stylesheet" href="{{ asset('packages/mrjuliuss/syntara/assets/css/base.css') }}" media="all">
 
         <script src="{{ asset('packages/mrjuliuss/syntara/assets/js/jquery-2.0.3.min.js') }}"></script>
