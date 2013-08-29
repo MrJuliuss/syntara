e12998ec12e9251f496dc5e3ee7ed66d06061360
Dan Sullivan <dan@embu.co.uk>
2013-08-29 12:38:22 +0100

    add else to navPages / navPagesRight

--- src/views/layouts/dashboard/header.blade.php
+++ src/views/layouts/dashboard/header.blade.php
@@ -17,7 +17,7 @@
             <ul class="nav navbar-nav">
                 <li class=""><a href="{{ URL::to('dashboard'); }}"><i class="glyphicon glyphicon-home"></i> <span>Dashboard</span></a></li>
                 @if (Sentry::check())
-                    {{ (!empty($navPages)) ? $navPages }}
+                    {{ (!empty($navPages)) ? $navPages : '' }}
                     @if($currentUser->hasAccess('view-users-list') || $currentUser->hasAccess('groups-management'))
                     <li class="dropdown" >
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i> <span>Users</span></a></a>
@@ -37,8 +37,8 @@
 
             @if(Sentry::check())
             <ul class="nav navbar-nav navbar-right">
-                {{ (!empty($navPagesRight)) ? $navPagesRight }}
                 <li class="hidden-sm"><img class="ajax-loader ajax-loader-lg" src="{{ asset('packages/mrjuliuss/syntara/assets/img/ajax-load.gif') }}" style="float: right;"/></li>
+                {{ (!empty($navPagesRight)) ? $navPagesRight : '' }}
                 <li><a href="{{ URL::route('showUser', Sentry::getUser()->id ) }}"><span class="text">{{ Sentry::getUser()->username }}</span></a></li>
                 <li><a title="Logout" href="{{ URL::to('dashboard/logout'); }}"><i class="glyphicon glyphicon-share-alt"></i> <span class="text">Logout</span></a></li>
             </ul>
