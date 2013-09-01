a4b123a8aaab8b386b0e45ccc05e32279f3e7e37
MrJuliuss <julien.richarte@gmail.com>
2013-09-01 19:10:56 +0200

    [Upg] add confirm modal on delete items

---
+++ src/views/layouts/dashboard/confirmation-modal.blade.php
@@ -0,0 +1,15 @@
+<div class="modal fade {{ $type }}" id="confirm-modal" tabindex="-1" role="dialog" aria-labelledby="confirm-modal" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">{{ $title }}</h4>
+            </div>
+            <div class="modal-body">{{ $content }}</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                <button type="button" class="btn btn-primary confirm-action">Yes</button>
+            </div>
+        </div>
+    </div>
+</div>
