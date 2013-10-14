f2e28850913dfea1d914ed79fd63631aed398c53
MrJuliuss <julien.richarte@gmail.com>
2013-10-14 21:09:19 +0200

    [fix] last_name / first_name validators now use alpha_dash

--- src/config/validator.php
+++ src/config/validator.php
@@ -4,8 +4,8 @@ $rules = array(
     'email' => array('required', 'email'),
     'pass' => array('required', 'min:6', 'max:18'),
     'username' => array('required', 'min:3', 'max:16', 'alpha'),
-    'last_name' => array('min:3', 'max:16', 'alpha'),
-    'first_name' => array('min:3', 'max:16', 'alpha'),
+    'last_name' => array('min:3', 'max:16', 'alpha_dash'),
+    'first_name' => array('min:3', 'max:16', 'alpha_dash'),
     'groupname' => array('required', 'min:3', 'max:16', 'alpha'),
     'permission' => array('required', 'min:3', 'max:32')
 );
