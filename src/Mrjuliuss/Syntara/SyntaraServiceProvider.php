fc18e73ab75f74ef00aa2abeb3a091df651707d2
Graham Campbell <graham@mineuk.com>
2013-11-28 22:47:45 +0000

    Tweaked comments

--- src/Mrjuliuss/Syntara/SyntaraServiceProvider.php
+++ src/Mrjuliuss/Syntara/SyntaraServiceProvider.php
@@ -30,7 +30,7 @@ class SyntaraServiceProvider extends ServiceProvider
     */
     public function register()
     {
-        //Load package config
+        // load package config
         $this->app['config']->package('mrjuliuss/syntara', __DIR__.'/../../config');
 
         $this->app['config']->set('view.pagination', 'syntara::layouts.pagination.bootstrap3');
@@ -59,7 +59,7 @@ class SyntaraServiceProvider extends ServiceProvider
         // register models
         $this->registerModels();
         
-        //Add commands
+        // add commands
         $this->commands('create:user');
         $this->commands('syntara:install');
         $this->commands('syntara:update');
@@ -80,12 +80,13 @@ class SyntaraServiceProvider extends ServiceProvider
     */
     public function registerHelpers()
     {
+        // register breadcrumbs
         $this->app['breadcrumbs'] = $this->app->share(function()
         {
             return new \MrJuliuss\Syntara\Helpers\Breadcrumbs();
         });
         
-        // Shortcut so developers don't need to add an Alias in app/config/app.php
+        // shortcut so developers don't need to add an Alias in app/config/app.php
         $this->app->booting(function()
         {
             $loader = \Illuminate\Foundation\AliasLoader::getInstance();
@@ -95,11 +96,13 @@ class SyntaraServiceProvider extends ServiceProvider
 
     public function registerModels()
     {
+        // register permission provider
         $this->app['permissionProvider'] = $this->app->share(function()
         {
             return new \MrJuliuss\Syntara\Models\Permissions\PermissionProvider();
         });
         
+        // add permission provider to aliases
         $this->app->booting(function()
         {
             $loader = \Illuminate\Foundation\AliasLoader::getInstance();
