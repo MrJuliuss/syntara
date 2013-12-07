18adb35a0d411d3d936c5796c63206a2e1cf0c6d
santi <nimblexworm@gmail.com>
2013-12-07 14:31:06 -0500

    Added Spanish language

---
+++ src/lang/es/groups.php
@@ -0,0 +1,21 @@
+<?php return array(
+    'all' => 'Todos los grupos',
+    'id' => 'Grupo id',
+    'name' => 'Nombre del grupo',
+    'add-user' => 'Agregar usuario',
+    'new' => 'Nuevo grupo',
+    'groups-users-title' => 'Usuarios en este grupo',
+    'messages' => array(
+        'exists' => 'El grupo con este nombre ya existe.',
+        'not-found' => 'Lo sentimos, el grupo no existe!',
+        'success' => 'Grupo modificado con exito.',
+        'try' => 'No se puede modificar este grupo, por favor vuelve a intentarlo.',
+        'delete-success' => 'Grupo eliminado con exito.',
+        'user-removed-success' => 'Usuario eliminado exitosamente.',
+        'user-add-success' => 'Usuario agregado correctamente.',
+    ),
+    'titles' => array(
+        'list' => 'Grupos',
+        'new' => 'Nuevo grupo',
+    ),
+);
---
+++ src/lang/es/navigation.php
@@ -0,0 +1,9 @@
+<?php 
+
+return array(
+    'index' => 'Tablero de Mando',
+    'users' => 'Usuarios',
+    'groups' => 'Grupos',
+    'permissions' => 'Permisos',
+    'logout' => 'Logout'
+);
