6da956fc3dc7f9e6f4ea125b5a559661322acbb8
Julien Richarte <julien.richarte@gmail.com>
2014-01-08 18:55:51 +0100

    [Upg] expends user validation to 255 chars

--- src/config/validator.php
+++ src/config/validator.php
@@ -4,21 +4,21 @@ return array(
     'user' => array(
         'create' => array(
                 'email' => array('required', 'email'),
-                'pass' => array('required', 'min:6', 'max:18'),
-                'username' => array('required', 'min:3', 'max:16', 'alpha'),
-                'last_name' => array('min:3', 'max:16', 'alpha_dash'),
-                'first_name' => array('min:3', 'max:16', 'alpha_dash'),
+                'pass' => array('required', 'min:6', 'max:255'),
+                'username' => array('required', 'min:3', 'max:255', 'alpha'),
+                'last_name' => array('min:3', 'max:255', 'alpha_dash'),
+                'first_name' => array('min:3', 'max:255', 'alpha_dash'),
             ),
         'update' => array(
             'email' => array('required', 'email'),
-            'pass' => array('min:6', 'max:18'),
-            'username' => array('required', 'min:3', 'max:16', 'alpha'),
-            'last_name' => array('min:3', 'max:16', 'alpha_dash'),
-            'first_name' => array('min:3', 'max:16', 'alpha_dash'),
+            'pass' => array('min:6', 'max:255'),
+            'username' => array('required', 'min:3', 'max:255', 'alpha'),
+            'last_name' => array('min:3', 'max:255', 'alpha_dash'),
+            'first_name' => array('min:3', 'max:255', 'alpha_dash'),
             ),
         'login' => array(
                 'email' => array('required', 'email'),
-                'pass' => array('required', 'min:6', 'max:18'),
+                'pass' => array('required', 'min:6', 'max:255'),
             ),
     ),
     'group' => array(
