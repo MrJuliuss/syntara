dd3af94a677863e9cb01d0b227a1110604d5fa5e
Nille af Ekenstam <n@nille.org>
2014-01-09 22:46:54 +0100

    Added Swedish language.

---
+++ src/lang/se/groups.php
@@ -0,0 +1,21 @@
+<?php return array(
+    'all' => 'Alla grupper',
+    'id' => 'Grupp-id',
+    'name' => 'Gruppnamn',
+    'add-user' => 'Lägg till användare',
+    'new' => 'Ny grupp',
+    'groups-users-title' => 'Användare i denna grupp',
+    'messages' => array(
+        'exists' => 'Grupp med detta namn existerar redan.',
+        'not-found' => 'Ledsen, gruppen hittas inte!',
+        'success' => 'Gruppen uppdaterades utan problem.',
+        'try' => 'Kan inte uppdatera denna grupp, vänligen försök igen.',
+        'delete-success' => 'Grupp raderad utan problem.',
+        'user-removed-success' => 'Användaren bortplockad ur gruppen utan problem.',
+        'user-add-success' => 'Användaren lades till i gruppen utan problem.',
+    ),
+    'titles' => array(
+        'list' => 'Grupplista',
+        'new' => 'Ny grupp',
+    ),
+);
