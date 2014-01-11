6f8298fb26bd0f6eef4c917c71ad26f7bdbd62ec
Athanasios Eleftheriadis <athanasios.eleftheriadis@gmail.com>
2014-01-11 10:14:03 +0200

    Greek Translation

---
+++ src/lang/el/breadcrumbs.php
@@ -0,0 +1,10 @@
+<?php return array(
+    'dashboard' => 'Το ταμπλό μου',
+    'login' => 'Σύνδεση',
+    'groups' => 'Ομάδες',
+    'new-group' => 'Νέα ομάδα',
+    'permissions' => 'Άδειες',
+    'new-permission' => 'Νέα άδεια',
+    'users' => 'Χρήστες',
+    'new-user' => 'Νέος χρήστης',
+);
