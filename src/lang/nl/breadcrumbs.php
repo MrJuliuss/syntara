bc21242f324c198b8457df54bdd40a966082677d
Daniel Koop <daniel@yourwebhoster.eu>
2014-01-14 07:43:28 +0100

    NL translation
    NL translation

---
+++ src/lang/nl/breadcrumbs.php
@@ -0,0 +1,10 @@
+<?php return array(
+    'dashboard' => 'Mijn Dashboard',
+    'login' => 'Login',
+    'groups' => 'Groepen',
+    'new-group' => 'Nieuwe groep',
+    'permissions' => 'Rechten',
+    'new-permission' => 'Nieuwe rechten',
+    'users' => 'Gebruikers',
+    'new-user' => 'Nieuwe gebruiker',
+);
---
+++ src/lang/nl/permissions.php
@@ -0,0 +1,19 @@
+<?php return array(
+    'permissions' => 'Toegangsrechten',
+    'all' => 'Alle toegangsrechten',
+    'id' => 'Toegangsrechten ID',
+    'value' => 'Waarde',
+    'new' => 'Nieuwe toegangsrechten',
+    'description' => 'Beschrijving',
+    'messages' => array(
+        'exists' => 'Er bestaan al toegangsrechten met deze waarde.',
+        'not-found' => 'De toegangsrechten zijn niet gevonden.',
+        'update-success' => 'De toegangsrechten zijn bijgewerkt.',
+        'update-fail' => 'Kan de toegangsrechten niet bijwerken, probeer het opnieuw.',
+        'remove-success' => 'De toegangsrechten zijn verwijderd.',
+    ),
+    'titles' => array(
+        'list' => 'Toegangsrechten lijst',
+        'new' => 'Nieuwe toegangsrechten',
+    ),
+);
