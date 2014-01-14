7cce704afdb70b9b191676cd6214e2b1a12e25fa
Serkan <serkan.bektas@wandadigital.com>
2014-01-14 11:57:09 +0200

    turkish language package from SerkanBektas

---
+++ src/lang/tr/groups.php
@@ -0,0 +1,21 @@
+<?php return array(
+    'all' => 'Tüm Gruplar',
+    'id' => 'Grup id',
+    'name' => 'Grup name',
+    'add-user' => 'Kullanıcı Ekle',
+    'new' => 'Yeni Grup',
+    'groups-users-title' => 'Bu Grubun Kullanıcıları',
+    'messages' => array(
+        'exists' => 'Bu isimle grup zaten var.',
+        'not-found' => 'Grup bulunamadı',
+        'success' => 'Grup başarılı bir şekilde düzenlendi.',
+        'try' => 'İşlem gerçekleştirilemedi, lütfen tekrar deneyin.',
+        'delete-success' => 'Grup başarılı şekilde taşındı.',
+        'user-removed-success' => 'Kullanıcı bu gruptan başarılı şekilde silindi.',
+        'user-add-success' => 'Kullanıcı gruba başarılı şekilde eklendi.',
+    ),
+    'titles' => array(
+        'list' => 'Grup Listesi',
+        'new' => 'Yeni Grup',
+    ),
+);
