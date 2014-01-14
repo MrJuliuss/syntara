6265f85ac69988c7c4dadea4b13bda47c10b1329
Daniel Koop <daniel@yourwebhoster.eu>
2014-01-14 07:43:28 +0100

    NL translation
    NL translation

---
+++ src/lang/nl/validation.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    'required'         => "Dit veld moet ingevuld worden.",
+    'email'            => "Dit e-mail adres is ongeldig.",
+    'alpha_dash'       => "In dit veld zijn alleen letters, nummers en koppeltekens toegestaan.",
+    'alpha'            => "In dit veld zijn alleen letters toegestaan.",
+    'last_name.min'    => "De achernaam moet uit minimaal :min karakters bestaan.",
+    'last_name.max'    => "De achernaam mag uit maximaal :max karakters bestaan.",
+    'first_name.min'   => "De naam moet uit minimaal :min karakters bestaan",
+    'first_name.max'   => "De naam mag uit maximaal :max karakters bestaan.",
+    'username.min'     => "De gebruikersnaam moet uit minimaal :min karakters bestaan.",
+    'username.max'     => "De gebruikersnaam mag uit maximaal :max karakters bestaan.",
+    'pass.min'         => "Het wachtwoord moet uit minimaal :min karakters bestaan.",
+    'pass.max'         => "Het wachtwoord mag uit maximaal :max karakters bestaan.",
+    'groupname.min'    => "De groepsnaam moet uit minimaal :min karakters bestaan.",
+    'groupname.max'    => "De groepsnaam mag uit maximaal :max karakters bestaan.",
+    'name.min'         => "De naam moet uit minimaal :min karakters bestaan.",
+    'name.max'         => "De naam moet uit mag :max karakters bestaan.",
+    'value.min'        => "De waarde moet uit minimaal :min karakters bestaan.",
+    'value.max'        => "De waarde moet uit mag :max karakters bestaan.",
+    'description.min'  => "De beschrijving moet uit minimaal :min karakters bestaan.",
+    'description.max'  => "De beschrijving moet uit mag :max karakters bestaan.",
+);
