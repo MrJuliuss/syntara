6565f46ef3271914cbfe8e2428b99ef7707b647f
julien <julien.richarte@gmail.com>
2014-06-10 22:00:22 +0200

    [Upg] jQuery 2.1.1

--- src/views/layouts/dashboard/master.blade.php
+++ src/views/layouts/dashboard/master.blade.php
@@ -16,7 +16,7 @@
         <link rel="icon" {{ !empty($faviconType) ? 'type="$faviconType"' : '' }} href="{{ $favicon }}" />
         @endif
 
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js"></script>
         <script src="{{ asset('packages/mrjuliuss/syntara/assets/js/dashboard/base.js') }}"></script>
 
