80711279ce477aaa68ffd427e04133d68d16ce4f
boyaq <boyaq@otkur.biz>
2014-08-21 17:30:37 +0800

    added Chinese translation

---
+++ src/lang/cn/validation.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    'required'         => "该字段不能为空。",
+    'email'            => "该邮件地址不正确。",
+    'alpha_dash'       => "该字段只能包括拉丁字符、数字和下划线。",
+    'alpha'            => "该字段只能由拉丁字符组成。",
+    'last_name.min'    => "姓必须多于:min字符。",
+    'last_name.max'    => "姓必须多于:max字符。",
+    'first_name.min'   => "名必须多于:min字符。",
+    'first_name.max'   => "名必须多于:max字符。",
+    'username.min'     => "用户名必须多于:min字符。",
+    'username.max'     => "用户名必须多于:max字符。",
+    'pass.min'         => "密码必须多于:min字符。",
+    'pass.max'         => "密码必须多于:max字符。",
+    'groupname.min'    => "用户组必须多于:min字符。",
+    'groupname.max'    => "用户组必须多于:max字符。",
+    'name.min'         => "名称必须多于:min字符。",
+    'name.max'         => "名称必须多于:max字符。",
+    'value.min'        => "权限值必须多于:min字符。",
+    'value.max'        => "权限值必须多于:max字符。",
+    'description.min'  => "介绍必须多于:min字符。",
+    'description.max'  => "介绍必须多于:max字符。",
+);
