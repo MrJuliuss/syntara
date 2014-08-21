81ab5b94e90a65d150f401853e77febe6803430b
boyaq <boyaq@otkur.biz>
2014-08-21 17:30:37 +0800

    added Chinese translation

---
+++ src/lang/cn/breadcrumbs.php
@@ -0,0 +1,10 @@
+<?php return array(
+    'dashboard' => '控制面板',
+    'login' => '登录',
+    'groups' => '用户组',
+    'new-group' => '新组',
+    'permissions' => '权限',
+    'new-permission' => '新权限',
+    'users' => '用户',
+    'new-user' => '新用户',
+);
---
+++ src/lang/cn/navigation.php
@@ -0,0 +1,9 @@
+<?php 
+
+return array(
+    'index' => '控制面板',
+    'users' => '用户',
+    'groups' => '用户组',
+    'permissions' => '权限',
+    'logout' => '退出'
+);
