4e43763bf440c2e14da2d0462d3af7e94083e87b
boyaq <boyaq@otkur.biz>
2014-08-21 17:30:37 +0800

    added Chinese translation

---
+++ src/lang/cn/permissions.php
@@ -0,0 +1,19 @@
+<?php return array(
+    'permissions' => '权限',
+    'all' => '全部权限',
+    'id' => '权限变化',
+    'value' => '值',
+    'new' => '新权限',
+    'description' => '介绍',
+    'messages' => array(
+        'exists' => '该权限已存在。',
+        'not-found' => '对不起，该权限不存在。',
+        'update-success' => '该权限已成功更新。',
+        'update-fail' => '暂时无法更新权限，请尝试。',
+        'remove-success' => '该权限已成功删除',
+    ),
+    'titles' => array(
+        'list' => '权限列表',
+        'new' => '新权限',
+    ),
+);
